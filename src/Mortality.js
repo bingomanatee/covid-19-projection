4df435a65c5b806ce5327a895d3f3d0f05d49326
David Edelhart <dave@wonderlandlabs.com>
2021-01-09 13:50:52 -0800

    no pads

--- src/Mortality.js
+++ src/Mortality.js
@@ -5,9 +5,9 @@ import Graph from './Graph';
 
 export default () => (
   <Grid rows={['auto', '1fr']} fill="true">
-    <Box pad={"medium"}>
+    <Box>
       <H textAlign="center" weight={800} level={1}>Mortality Rates</H>
-      <P>
+      <P align="center">
         This page is an up-do-date graph on COVID-19 Mortality rates in the United States of America.
         Deaths past current date are based on a linear extrapolation of deaths over time
         based on the progress of deaths since a month ago.
